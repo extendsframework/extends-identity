7ebcc3b85974cc15a79440f834b5bf1ebb11bd90
Vincent van Dijk <info@vincentvandijk.com>
2017-12-27 19:47:51 +0100

    Changed return type.

--- src/Framework/ServiceLocator/Factory/StorageFactory.php
+++ src/Framework/ServiceLocator/Factory/StorageFactory.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 namespace ExtendsFramework\Identity\Framework\ServiceLocator\Factory;
 
 use ExtendsFramework\Identity\Storage\InMemory\InMemoryStorage;
-use ExtendsFramework\Identity\Storage\StorageInterface;
 use ExtendsFramework\ServiceLocator\Resolver\Factory\ServiceFactoryInterface;
 use ExtendsFramework\ServiceLocator\ServiceLocatorInterface;
 
@@ -13,7 +12,7 @@ class StorageFactory implements ServiceFactoryInterface
     /**
      * @inheritDoc
      */
-    public function createService(string $key, ServiceLocatorInterface $serviceLocator, array $extra = null): StorageInterface
+    public function createService(string $key, ServiceLocatorInterface $serviceLocator, array $extra = null): object
     {
         return new InMemoryStorage();
     }
